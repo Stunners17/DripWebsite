3960398740b54889675bb3b85477cd9412d203f2
Stunners17 <ntando.lawrance@gmail.com>
2026-07-12 09:30:25 +0200

    update

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDoF6r7eo6Sdk4ptkFwWh3plRTh5M8nWgM",
+  authDomain: "the-plug-store.firebaseapp.com",
+  projectId: "the-plug-store",
+  storageBucket: "the-plug-store.firebasestorage.app",
+  messagingSenderId: "445957804097",
+  appId: "1:445957804097:web:9d19ad93f281397cd376c0"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
